8a2dd6840a933ea8d7e6af351acf9cb17f37bb0c
Phuc <duyphuclenguyen@gmail.com>
2025-06-29 21:32:59 -0500

    Updated

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,10 @@
-const todoList = [];
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'play with kids',
+  dueDate: '2022-12-25'
+}]
 
 renderTodoList();
 
@@ -6,8 +12,16 @@ function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++){
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`
+    const todoObject = todoList[i];
+    const  { name, dueDate } = todoObject
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onClick="
+        todoList.splice(${i}, 1);
+        renderTodoList();"
+        class="delete-todo-button">Delete</button>
+    `;
     todoListHTML += html
   }
 
@@ -17,9 +31,17 @@ function renderTodoList() {
 
 function addToDo() {
   const inputElement = document.querySelector(".js-name-input")
+  const dateElement = document.querySelector(".js-due-date-input")
+
   const name = inputElement.value
-  todoList.push(name)
+  const dueDate = dateElement.value
+
+  todoList.push({
+    name, dueDate
+  })
+
   inputElement.value = '';
+  dateElement.value = '';
 
   renderTodoList();
 }
